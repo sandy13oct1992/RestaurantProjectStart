fcbe406fdfc5d52e035a843c7fd227ad9551f3cc
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-03-12 19:29:52 +0530

    Strting Restaurent Project

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Header = props => {
+ return <Fragment>
+    <header>
+     <h1>react Meals</h1>
+    </header>
+ </Fragment>
+}
+
+export default Header;
