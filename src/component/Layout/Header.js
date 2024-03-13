1a19c2539800dbc26c3b0aa6d82291645abec074
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-03-13 07:54:48 +0530

    project started

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
+import MealsImage from './Meals.jpg'
 
 const Header = props => {
  return <Fragment>
     <header>
      <h1>react Meals</h1>
+     <button>cart</button>
     </header>
+    <div>
+      <img src={MealsImage} alt="A Table full of delicious food" />
+    </div>
  </Fragment>
 }
 
