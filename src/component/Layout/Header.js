7d354f74ae5799a8076d9a4cc086b1dcb22b68a0
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-03-13 08:06:37 +0530

    Q.No.1

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import MealsImage from './Meals.jpg'
+import MealsImage from './Meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
  return <Fragment>
     <header>
      <h1>react Meals</h1>
-     <button>cart</button>
+     <HeaderCartButton/>
     </header>
     <div>
       <img src={MealsImage} alt="A Table full of delicious food" />
---
+++ src/component/Layout/HeaderCartButton.js
@@ -0,0 +1,12 @@
+
+const HeaderCartButton = props => {
+ return <button>
+    <span>
+        Your Cart
+    </span>
+    <span> 3 </span>
+
+ </button>
+}
+
+export default HeaderCartButton;
